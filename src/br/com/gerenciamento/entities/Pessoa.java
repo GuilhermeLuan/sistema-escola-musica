4466f2b5a130d81c34344cf2e03db17d310ec156
O-Gui <137916326+O-Gui@users.noreply.github.com>
2024-11-12 21:02:27 -0300

    classe base pessoa

--- src/br/com/gerenciamento/entities/Pessoa.java
+++ src/br/com/gerenciamento/entities/Pessoa.java
@@ -1,5 +1,70 @@
 package br.com.gerenciamento.entities;
 
 public class Pessoa {
+	
+	private String nome;
+    private int idade;
+    private String endereco;
+    private String cpf;
+    private String curso;
 
+    public Pessoa(String nome, int idade, String endereco, String cpf, String curso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+        this.cpf = cpf;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "Nome='" + nome + '\'' +
+                ", Idade=" + idade +
+                ", Endereco='" + endereco + '\'' +
+                ", CPF='" + cpf + '\'' +
+                ", Curso='" + curso + '\'' +
+                '}';
+    }
 }
