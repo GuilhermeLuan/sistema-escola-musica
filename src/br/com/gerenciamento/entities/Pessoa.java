6233486fde0d33dc8660c1692b207ca313292700
O-Gui <137916326+O-Gui@users.noreply.github.com>
2024-11-12 21:16:54 -0300

    atualizacao classe base pessoa

--- src/br/com/gerenciamento/entities/Pessoa.java
+++ src/br/com/gerenciamento/entities/Pessoa.java
@@ -1,4 +1,5 @@
 package br.com.gerenciamento.entities;
+import br.com.gerenciamento.entities.cursos.Curso;
 
 public class Pessoa {
 	
@@ -6,65 +7,66 @@ public class Pessoa {
     private int idade;
     private String endereco;
     private String cpf;
-    private String curso;
+    private Curso curso;
+	
+	public Pessoa(String nome, int idade, String endereco, String cpf, Curso curso) {
+		this.nome = nome;
+		this.idade = idade;
+		this.endereco = endereco;
+		this.cpf = cpf;
+		this.curso = curso;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public Curso getCurso() {
+		return curso;
+	}
+	
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+	
+	// metodo da classe base pessoa
+	@Override
+	public String toString() {
+		return "Pessoa{" +
+				"Nome='" + nome + '\'' +
+				", Idade=" + idade +
+				", Endereco='" + endereco + '\'' +
+				", CPF='" + cpf + '\'' +
+				", Curso=" + (curso != null ? curso.toString() : "Nenhum curso cadastrado") +
+				'}';
+	}
 
-    public Pessoa(String nome, int idade, String endereco, String cpf, String curso) {
-        this.nome = nome;
-        this.idade = idade;
-        this.endereco = endereco;
-        this.cpf = cpf;
-        this.curso = curso;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
-
-   
-    @Override
-    public String toString() {
-        return "Pessoa{" +
-                "Nome='" + nome + '\'' +
-                ", Idade=" + idade +
-                ", Endereco='" + endereco + '\'' +
-                ", CPF='" + cpf + '\'' +
-                ", Curso='" + curso + '\'' +
-                '}';
-    }
 }
