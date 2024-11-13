df42c6b03b2efaa6db12f5c59eaad28ec2bad087
gabrielvvs1 <gabrielvvs1@gmail.com>
2024-11-12 21:44:29 -0300

    classe professor

--- src/br/com/gerenciamento/entities/Professor.java
+++ src/br/com/gerenciamento/entities/Professor.java
@@ -1,5 +1,54 @@
 package br.com.gerenciamento.entities;
+import java.util.List;
 
-public class Professor {
+import br.com.gerenciamento.entities.cursos.Curso;
 
+public class Professor extends Pessoa { // hierarquia da classe Pessoa com a subclasse Professor
+	
+	// propriedade da subclasse Professor
+	private float Salario; 
+	private List<Professor> HorariosDisponivel;
+	
+	//construtor
+	public Professor(String nome, int idade, String endereco, String cpf, Curso curso, float salario,
+			List<Professor> horariosDisponivel) {
+		super(nome, idade, endereco, cpf, curso);
+		Salario = salario;
+		HorariosDisponivel = horariosDisponivel;
+	}
+	
+	
+	//encapsulamento //vvvvvvvvvv
+	public float getSalario() { 
+		return Salario;
+	}
+
+	public void setSalario(float salario) {
+		Salario = salario;
+	}
+
+	public List<Professor> getHorariosDisponivel() {
+		return HorariosDisponivel;
+	}
+
+	public void setHorariosDisponivel(List<Professor> horariosDisponivel) {
+		HorariosDisponivel = horariosDisponivel;
+	}
+
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString().replace("}", "")+
+				"Horário: " +HorariosDisponivel + '\''+
+				"Salário: " +Salario+
+				'}';
+	}
+
+ 
+	
+	
+	
+	
+	
 }
