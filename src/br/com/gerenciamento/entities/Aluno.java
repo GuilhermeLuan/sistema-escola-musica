10089e833a6767f47e2e2e9d63a07041dfc7c023
Larissa Kailane <larissakailanelv@gmail.com>
2024-11-12 21:38:49 -0300

    Primeira atualização da classe aluno - atributos, construtor e override do método toString

--- src/br/com/gerenciamento/entities/Aluno.java
+++ src/br/com/gerenciamento/entities/Aluno.java
@@ -1,5 +1,32 @@
 package br.com.gerenciamento.entities;
 
-public class Aluno {
+import br.com.gerenciamento.entities.cursos.Curso;
+
+//CLASSE FILHA - Aluno
+public class Aluno extends Pessoa {
+	//Atributo
+	private int matricula;
+	private String nivel;
+	
+	//Construtor
+	public Aluno(String nome, int idade, String endereco, String cpf, Curso curso, int matricula, String nivel) {
+		super(nome, idade, endereco, cpf, curso);
+		this.matricula = matricula;
+		this.nivel = nivel;
+	}
+
+	//Polimorfismo - sobrescrita do método toString
+	@Override
+	public String toString() {
+		return super.toString().replace("}", "")+
+		"Matricula: " +matricula+ '\''+
+		"Nível do aluno: " +nivel+
+		'}';
+		
+	}
+	
+	
+	
+	
 
 }
