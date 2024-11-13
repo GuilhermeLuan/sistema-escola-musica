2ea06a5b187d2a9d68f4c119708e9ae7b28f740b
O-Gui <137916326+O-Gui@users.noreply.github.com>
2024-11-12 21:32:00 -0300

    criando interface

--- src/br/com/gerenciamento/entities/Agendavel.java
+++ src/br/com/gerenciamento/entities/Agendavel.java
@@ -1,5 +1,14 @@
 package br.com.gerenciamento.entities;
 
 public interface Agendavel {
+	    
+	    // Método para agendar uma aula
+	    void agendarAula();
+	    
+	    // Método para cancelar uma aula
+	    void cancelarAula();
+	    
+	    // Método para visualizar aulas agendadas
+	    void visualizarAulas();
+	}
 
-}
