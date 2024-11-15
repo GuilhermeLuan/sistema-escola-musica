b2ee66ea6241f914d6b6bcf70f80c49a25c64e8e
O-Gui <137916326+O-Gui@users.noreply.github.com>
2024-11-15 16:35:11 -0300

    padrao observe & classe aula

--- src/br/com/gerenciamento/i/AgendaAulas.java
+++ src/br/com/gerenciamento/i/AgendaAulas.java
@@ -1,5 +1,13 @@
 package br.com.gerenciamento.i;
 
 public interface AgendaAulas {
-
+    
+    // Método para agendar uma aula
+    void agendarAula();
+    
+    // Método para cancelar uma aula
+    void cancelarAula();
+    
+    // Método para visualizar aulas agendadas
+    void visualizarAulas();
 }
