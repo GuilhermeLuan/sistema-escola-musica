2fe88e76b48477cd2d1852e30b67e06c5a0b8570
Guilherme Luan <guilhermeluan@pm.me>
2024-11-15 16:44:17 -0300

    feat: aula

--- src/br/com/gerenciamento/entities/Aula.java
+++ src/br/com/gerenciamento/entities/Aula.java
@@ -3,26 +3,51 @@ package br.com.gerenciamento.entities;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import br.com.gerenciamento.entities.curso.TipoCurso;
+
 public class Aula {
+	private TipoCurso curso;
     private LocalDate data;
     private LocalTime horaInicio;
     private LocalTime horaFim;
+    
+	public Aula(TipoCurso curso, LocalDate data, LocalTime horaInicio, LocalTime horaFim) {
+		super();
+		this.curso = curso;
+		this.data = data;
+		this.horaInicio = horaInicio;
+		this.horaFim = horaFim;
+	}
 
-    public Aula(LocalDate data, LocalTime horaInicio, LocalTime horaFim) {
-        this.data = data;
-        this.horaInicio = horaInicio;
-        this.horaFim = horaFim;
-    }
+	public TipoCurso getCurso() {
+		return curso;
+	}
 
-    public LocalDate getData() {
-        return data;
-    }
+	public void setCurso(TipoCurso curso) {
+		this.curso = curso;
+	}
 
-    public LocalTime getHoraInicio() {
-        return horaInicio;
-    }
+	public LocalDate getData() {
+		return data;
+	}
 
-    public LocalTime getHoraFim() {
-        return horaFim;
-    }
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public LocalTime getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(LocalTime horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public LocalTime getHoraFim() {
+		return horaFim;
+	}
+
+	public void setHoraFim(LocalTime horaFim) {
+		this.horaFim = horaFim;
+	}    
 }
